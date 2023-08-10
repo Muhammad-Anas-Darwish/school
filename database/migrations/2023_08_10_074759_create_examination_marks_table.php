22aae83b0fb9b69f518a5f8920232a8e06ad4c99
Muhammad-Anas-Darwish <muhammadanasdarwish1@gmail.com>
2023-08-10 11:50:02 +0300

    add migrations files

---
+++ database/migrations/2023_08_10_074759_create_examination_marks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('examination_marks', function (Blueprint $table) {
+            $table->id();
+            $table->integer('mark');
+            $table->foreignId('student_id')->constrained();
+            $table->foreignId('subject_id')->constrained();
+            $table->foreignId('class_id')->constrained(table: 'classes');
+            $table->foreignId('exam_id')->constrained();
+            $table->foreignId('year_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('examination_marks');
+    }
+};
---
+++ database/migrations/2023_08_10_074814_create_marks_year_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('marks_year', function (Blueprint $table) {
+            $table->id();
+            $table->integer('mark');
+            $table->foreignId('student_id')->constrained();
+            $table->foreignId('subject_id')->constrained();
+            $table->foreignId('class_id')->constrained(table: 'classes');
+            $table->foreignId('year_id')->constrained();
+            $table->foreignId('exam_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('marks_year');
+    }
+};
