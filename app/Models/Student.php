954704d27cd2ca95e5013288796d9a56f665f17c
Muhammad-Anas-Darwish <muhammadanasdarwish1@gmail.com>
2023-08-21 15:04:35 +0300

    add one to one relation

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Student extends Model
 {
@@ -11,17 +12,29 @@ class Student extends Model
 
     protected $fillable = ['first_name', 'last_name', 'father_name', 'mother_name', 'birth_date', 'gender', 'user_id', 'grade_id', 'classroom_id'];
 
-    public function user()
+    /**
+     * @return BelongsTo
+     * @description Get the user
+     */
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    public function grade()
+    /**
+     * @return BelongsTo
+     * @description Get The grade the student belongs to
+     */
+    public function grade(): BelongsTo
     {
         return $this->belongsTo(Grade::class);
     }
 
-    public function classroom()
+    /**
+     * @return BelongsTo
+     * @description Get The classroom the student belongs to
+     */
+    public function classroom(): BelongsTo
     {
         return $this->belongsTo(Classroom::class);
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -43,4 +44,13 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * @return HasOne
+     * @description get the student associated with the student
+     */
+    public function student(): HasOne
+    {
+        return $this->hasOne(Student::class);
+    }
 }
